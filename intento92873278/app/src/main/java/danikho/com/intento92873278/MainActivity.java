6772a6032058e0531f4effdc78a82d4509c8bf6f
DanielK2020 <danikhoseo@gmail.com>
2020-09-30 01:06:57 -0300

    cambio

---
+++ intento92873278/app/src/main/java/danikho/com/intento92873278/MainActivity.java
@@ -0,0 +1,14 @@
+package danikho.com.intento92873278;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
